6e57158d1adc03671f04ca38952f3f8cccccac57
sleepysleepyfox <shkrebnsh@gmail.com>
2023-04-04 14:20:31 +0300

    minor changes to style and truncate text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import "./App.css"
 import { Navbar, Nav, Container, ThemeProvider, InputGroup, Button, Form, CloseButton, Dropdown } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
@@ -7,6 +8,7 @@ import { addtodo } from './features/todoSlice'
 import DropdownItem from 'react-bootstrap/esm/DropdownItem'
 import DropdownMenu from 'react-bootstrap/esm/DropdownMenu'
 import DropdownToggle from 'react-bootstrap/esm/DropdownToggle'
+import { useIsOverflow } from './hooks/useIsOverflow '
 
 
 
@@ -15,22 +17,25 @@ export default function App() {
   const [input, setInput] = useState('')
   const task = useSelector(selectAllValue)
 
+  const ref = React.useRef();
+  const isOverflow = useIsOverflow(ref);
+
   function addTask(){
     input != "" && store.dispatch(addtodo(input))
     console.log(input)
   }
-
+  
   const display = (taskstorage) => (
     taskstorage.map(e => (
-      <Container className='w-50 p-1 mb-t-2 mt-2 h-25 d-flex justify-content-between border'>
-          {e.taskContent.length < 26 ? 
-          <h1 className='text-truncate'>{e.taskContent}</h1> :
-          <Dropdown className='w-100 d-flex justify-content-between'>
-            <DropdownToggle className='bg-none border-color-light'>
-              <h1 className='text-truncate'>{e.taskContent}</h1>
+      <Container className='w-50 p-2 mb-t-2 mt-2 h-25 d-flex flex-grow-1 justify-content-between bg-light rounded'>
+          {e.taskContent.length < 100 ? 
+          <h1 ref={ref}>{e.taskContent}</h1> :
+          <Dropdown className='w-100 dropdown-colo' style={{background: "transparant"}}>
+            <DropdownToggle className='w-100 dropdown-color btn-light '>
+              <h1 className='text-truncate' style={{color: 'black'}}>{e.taskContent}</h1>
             </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>
+            <DropdownMenu className='w-100 wrap-dropdown'> 
+              <DropdownItem className='w-100 text-break wrap-dropdown btn-light'>
                 {e.taskContent}
               </DropdownItem>
             </DropdownMenu>
